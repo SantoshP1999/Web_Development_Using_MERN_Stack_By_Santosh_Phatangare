0346768e1a0590fa4c17bdec398a90f05356885a
SantoshP1999 <santoshphatangare12@gmail.com>
2024-08-12 10:43:46 +0530

    12-08-2024 Simple Registration

---
+++ 03_Simple_Registration/action.js
@@ -0,0 +1,6 @@
+function submitUserInformation(){
+    var userName=document.getElementById("username_input");
+    var password=document.getElementById("password_input");
+
+    alert("Username: " + userName.value + " Password: " + password.value);
+}
