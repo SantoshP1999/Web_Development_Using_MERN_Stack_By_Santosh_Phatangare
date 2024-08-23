ebed18cba934e2d7024495c3e78c3fa4e2b3a273
SantoshP1999 <santoshphatangare12@gmail.com>
2024-08-23 08:30:22 +0530

    23-08-2024 Friday Data Definations in JavaScript

---
+++ 09_DataDefinationInJS/script.js
@@ -0,0 +1,13 @@
+
+// Data Defination Statement
+
+let num=10;
+var iNum;
+
+console.log(num);
+
+var title=document.querySelector("h1");
+var heading2=document.querySelector("h2");
+
+title.textContent="Hello Students";
+heading2.textContent=num;
