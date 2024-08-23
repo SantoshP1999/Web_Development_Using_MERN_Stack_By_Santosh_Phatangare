9c4536018558c1119157a091c919352b13736ccc
SantoshP1999 <santoshphatangare12@gmail.com>
2024-08-23 08:29:24 +0530

    22-08-2024 Thursday Callback Functions

---
+++ 08_Callback_Functions/script.js
@@ -0,0 +1,7 @@
+
+var elmButton=document.querySelector('button')
+var elmHeading1=document.querySelector('h1')
+
+elmButton.addEventListener('click',function () {
+    elmHeading1.textContent='THE BUTTON IS CLICKED'
+})
